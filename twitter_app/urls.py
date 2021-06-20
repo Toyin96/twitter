7ee5a714fb1bb14cced25dd5725ac1770b6ab642
Toyin96 <onagoruwam@gmail.com>
2021-06-20 22:32:20 +0100

    main is updated

--- twitter_app/urls.py
+++ twitter_app/urls.py
@@ -6,5 +6,4 @@ app_name = 'twitter_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path("new_tweet/", views.create_tweet, name="create_tweet"),
-    path("accounts/profile/", views.home, name='home'),
 ]
